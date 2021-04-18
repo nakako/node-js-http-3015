4bd0dc95e89c0c72e7a4a6971eed8b47c8fa967e
nakako <nakako.soft@gmail.com>
2021-04-18 20:54:04 +0900

    ログから日付を除去

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const http = require('http');
 const pug = require('pug');
 const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  console.info('Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
@@ -70,7 +69,7 @@ const server = http.createServer((req, res) => {
         const qs = require('querystring');
         const answer = qs.parse(rawData);
         const body = answer['name'] + 'さんは' + answer['favorite'] + 'に投票しました';
-        console.info('[' + now + '] ' + body);
+        console.info(body);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
           body + '</h1></body></html>');
       });
@@ -80,14 +79,13 @@ const server = http.createServer((req, res) => {
       break;
   }
 
-
 }).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
+  console.error('Server Error', e);
 }).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  console.error('Client Error', e);
 });
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('Listening on ' + port);
 });
